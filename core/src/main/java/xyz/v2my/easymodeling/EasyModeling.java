13541344874bdf858825842d8fdce655b33e1b9b
Zhe Zhang <zhezhang@outlook.de>
2021-11-28 16:27:03 +0100

    add sub-project example and refine project structure

---
+++ core/src/main/java/xyz/v2my/easymodeling/EasyModeling.java
@@ -0,0 +1,8 @@
+package xyz.v2my.easymodeling;
+
+public class EasyModeling {
+
+    public static <T> T generate(Class<T> clazz) {
+        return null;
+    }
+}
---
+++ example/src/main/java/xyz/v2my/easymodeling/Example.java
@@ -0,0 +1,9 @@
+package xyz.v2my.easymodeling;
+
+public class Example {
+
+    public static void main(String[] args) {
+        final String generate = EasyModeling.generate(String.class);
+    }
+
+}
