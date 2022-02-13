aef18a1a9ecc2ff906599409d47e8b47af2f2c2b
Zhe Zhang <zhezhang@outlook.de>
2022-02-14 00:00:44 +0100

    fix: remove useless typeName from FieldDeclaration

--- processor/src/main/java/xyz/v2my/easymodeling/modeler/FieldDeclaration.java
+++ processor/src/main/java/xyz/v2my/easymodeling/modeler/FieldDeclaration.java
@@ -1,15 +1,11 @@
 package xyz.v2my.easymodeling.modeler;
 
-import com.squareup.javapoet.TypeName;
-
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 
 public class FieldDeclaration {
 
     private String fieldName;
-
-    private TypeName typeName;
 
     private TypeMirror typeMirror;
 
@@ -19,16 +15,11 @@ public class FieldDeclaration {
 
     public FieldDeclaration(VariableElement element) {
         this.fieldName = element.getSimpleName().toString();
-        this.typeName = TypeName.get(element.asType());
         this.typeMirror = element.asType();
     }
 
     public String getFieldName() {
         return fieldName;
-    }
-
-    public TypeName getTypeName() {
-        return typeName;
     }
 
     public TypeMirror getTypeMirror() {
