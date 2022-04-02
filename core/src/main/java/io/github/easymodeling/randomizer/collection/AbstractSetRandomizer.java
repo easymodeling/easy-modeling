6d59f6559da241589b697de5ad8044b395289bb7
Zhe Zhang <zhezhang@outlook.de>
2022-04-02 22:14:38 +0200

    Refactory: remove some redundant implementation

--- core/src/main/java/io/github/easymodeling/randomizer/collection/AbstractSetRandomizer.java
+++ core/src/main/java/io/github/easymodeling/randomizer/collection/AbstractSetRandomizer.java
@@ -3,16 +3,10 @@ package io.github.easymodeling.randomizer.collection;
 import io.github.easymodeling.randomizer.Randomizer;
 
 import java.util.Set;
-import java.util.stream.Stream;
 
 public abstract class AbstractSetRandomizer<C extends Set<E>, E> extends CollectionRandomizer<C, E> {
 
     protected AbstractSetRandomizer(Randomizer<E> elementRandomizer, int maxSize) {
         super(elementRandomizer, 1, maxSize);
-    }
-
-    @Override
-    protected Stream<E> upstream() {
-        return originalStream().distinct();
     }
 }
